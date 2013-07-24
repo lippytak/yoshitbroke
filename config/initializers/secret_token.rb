977a82c3e5032d22b0be828d329f3a53f8bda8e4
Jake Solomon <jacob@codeforamerica.org>
2013-07-24 08:44:46 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoshitbroke::Application.config.secret_token = 'e9f4248e7996869fb179805ce5e7590b68ab43375e14b322305f9135338006c8e08ef4e98897925cd6a9f44d07d5b8b9a35f993769413f5767328fa4aea41d18'
